778f1df429858c23aff2cedf5fa8cbb8efe20d57
ces08 <christinesong08@gmail.com>
2026-02-23 18:30:10 -0500

    congressional representative tab transition added

--- client/src/components/StateDetail.jsx
+++ client/src/components/StateDetail.jsx
@@ -1,12 +1,23 @@
 import { ChevronRightIcon, ChevronLeftIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
 import {UserIcon} from '@heroicons/react/24/solid'
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
 
 export default function StateDetail({expanded, onClick}){
     
     const [repDetail, setRepDetail] = useState(false);
-    
+    const repTopRef = useRef(null);
+
+    function toReps(){
+        setRepDetail(true);
+        repTopRef.current.scrollTo({ top: 0 });
+    }
+
+    function toDef(){
+        setRepDetail(false);
+        repTopRef.current.scrollTo({ top: 0 });
+    }
+
     const races = [
         {race: "White", percent: 39.78, popNumber: 700000},
         {race: "Black", percent: 20.14, popNumber: 300000},
@@ -22,16 +33,15 @@ export default function StateDetail({expanded, onClick}){
      const reps = [
         {repName: "Bob Dylan", party: 'republican'},
         {repName: "Bob Dylan", party: 'republican'},
-        {repName: "Bob Dylan", party: 'democrat'},
-        {repName: "Bob Dylan", party: 'democrat'},
+        {repName: "Bob Dylan", party: 'democratic'},
+        {repName: "Bob Dylan", party: 'democratic'},
         {repName: "Bob Dylan", party: 'republican'},
         {repName: "Bob Dylan", party: 'republican'},
-        {repName: "Bob Dylan", party: 'democrat'},
+        {repName: "Bob Dylan", party: 'democratic'},
         {repName: "Bob Dylan", party: 'republican'},
-        {repName: "Bob Dylan", party: 'democrat'},
-        {repName: "Bob Dylan", party: 'democrat'},
-       
-    ];
+        {repName: "Bob Dylan", party: 'democratic'},
+        {repName: "Bob Dylan", party: 'democratic'},
+    ]
 
     const partyControl = "Democrat";
 
@@ -46,20 +56,31 @@ export default function StateDetail({expanded, onClick}){
                 { expanded ? <ChevronUpIcon className = 'w-5'/> : <ChevronDownIcon className = 'w-5'/> }
             </div>
 
-            <div className={`flex flex-col gap-10 px-5 overflow-y-scroll ${expanded ? 'opacity-100 pt-5' : 'max-h-0 opacity-0'}`}>
-                
-                {/* <div className = {`${repDetail? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'}`}> */}
-                    <DefaultDetail races = {races} stateVoterDist = {stateVoterDist} partyControl = {partyControl}/>
-                {/* </div> */}
+            <div ref = {repTopRef} className={`relative gap-10 px-5 overflow-y-scroll overflow-x-hidden ${expanded ? 'opacity-100' : 'max-h-0 opacity-0'}`}>
+                <div className = {` ${repDetail ? 'slideInR': 'slideInL'}`}>
 
-                <div className = 'group flex text-gray-500 justify-between cursor-pointer hover:bg-gray-100 -mx-5 p-5' onClick = {()=> setRepDetail(true)}>
-                    <span className = 'group-hover:translate-x-2'>Congressional Representatives</span>
-                    <ChevronRightIcon className = 'w-5 group-hover:translate-x-2 '/>
+                {!repDetail ? 
+                    (<div className = 'flex flex-col gap-5'>
+                        <DefaultDetail races = {races} stateVoterDist = {stateVoterDist} partyControl = {partyControl}/>
+                        <div className = 'group flex text-gray-500 justify-between cursor-pointer hover:bg-gray-100 -mx-5 p-5' onClick = {toReps}>
+                            <span className = 'group-hover:translate-x-2'>Congressional Representatives</span>
+                            <ChevronRightIcon className = 'w-5 group-hover:translate-x-2 '/>
+                        </div>
+                    </div>)
+                
+                    :
+
+                    (<div className = 'flex flex-col'>
+                        <div className = 'group flex text-gray-500 justify-between cursor-pointer hover:bg-gray-100 -mx-5 p-5' onClick = {toDef}>
+                            <ChevronLeftIcon className = 'w-5 group-hover:-translate-x-2 '/>
+                            <span className = 'group-hover:-translate-x-2'>Congressional Representatives</span>
+                        </div>
+                        <CongressRepDetail repArr = {reps} />
+                        
+                    </div>)
+                }
                 </div>
 
-                {/* <div className = {`${repDetail ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'}`}> */}
-                    <CongressRepDetail repArr = {reps} />
-                {/* </div> */}
                 
             </div>
         </div>
@@ -114,7 +135,7 @@ function DefaultDetail({races, stateVoterDist, partyControl}){
 
     return(
     <>
-        <div className = 'flex text-gray-500 justify-between'>
+        <div className = 'flex text-gray-500 justify-between pt-5'>
             <span>State Population</span>
             <span className = "text-emerald-500 font-bold">3,200,000</span>
         </div>
@@ -136,9 +157,9 @@ function CongressRepDetail({repArr, onClick}){
         {repArr.map(({repName, party})=>(
             <div className = 'flex gap-2'>
                 <UserIcon className = 'w-8'/>
-                <div className = 'flex flex-col gap-2'>
+                <div className = 'flex flex-col gap-1'>
                     <div className = 'text-sm'>{repName}</div>
-                    <div className = 'text-xs capitalize'>{party} Party</div>
+                    <div className = {`text-xs capitalize ${party == "republican"? 'text-red-500' : 'text-blue-500'}`}>{party} Party</div>
                 </div>
             </div>
         ))}
