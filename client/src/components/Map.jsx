3aad7feb446a8629701c33a2122bff52c97a9095
lee-H1208 <leeh120824@gmail.com>
2026-02-24 17:56:09 -0500

    Added state lines geojson to public and added state lines to map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default function Map() {
+  const [stateLines, setStateLines] = useState(null);
   const usBounds = [
     [24.396308, -124.848974],
     [49.384358, -66.885444]   
   ];
+  
+  /* US State Lines Style */
+  const lineStyle = {
+    fillOpacity: 0,
+    weight: 1,
+    color: "#cccccc",
+  };
+
+  useEffect(() => {
+    fetch("/geojson/2024_us_state_lines.json")
+      .then((res) => res.json())
+      .then((data) => setStateLines(data))
+      .catch((err) => console.error("Error loading geojson:", err));
+  }, []);
 
   return (
     <MapContainer
       bounds={usBounds}
       className="fixed inset-0 h-screen w-full"
     >
-        <TileLayer
+      <TileLayer
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
-        />
+      />
+
+      {stateLines && <GeoJSON data={stateLines} style={lineStyle}/>}
     </MapContainer>
   );
 }
