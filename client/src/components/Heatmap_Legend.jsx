ec993c9b0bce3a2a6906ba73d8992eddba80d8c9
ces08 <christinesong08@gmail.com>
2026-03-05 21:22:55 -0500

    heat map legend title added

--- client/src/components/Heatmap_Legend.jsx
+++ client/src/components/Heatmap_Legend.jsx
@@ -1,8 +1,8 @@
 export default function Heatmap_Legend({title, items}){
 
     return(
-        <div className = 'fixed z-[1000] left-24 bottom-6 flex gap-2 flex-col bg-white rounded-md shadow-md items-start w-fit h-fit pl-4 pr-10 pb-3 pt-1'>
-            <div className = 'text-xl'>{title}</div>
+        <div className = 'fixed z-[1000] left-24 bottom-6 flex gap-2 flex-col bg-white rounded-md shadow-md items-start w-fit h-fit px-5 py-3'>
+            <div className = 'text-sm'>{title}</div>
             {items.map(({label, color})=>(
                 <div key = {label} className = 'flex gap-2 items-start'>
                     <div className = 'rounded w-4 h-4' style = {{backgroundColor: color}}/>
