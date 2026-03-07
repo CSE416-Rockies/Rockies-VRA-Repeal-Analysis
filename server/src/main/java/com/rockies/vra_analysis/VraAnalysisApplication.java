420d34dcc65084a72ed23d0bed2dacafcb1016eb
lee-H1208 <leeh120824@gmail.com>
2026-03-07 16:12:36 -0500

    Adding connection to mongodb

--- server/src/main/java/com/rockies/vra_analysis/VraAnalysisApplication.java
+++ server/src/main/java/com/rockies/vra_analysis/VraAnalysisApplication.java
@@ -1,13 +1,30 @@
 package com.rockies.vra_analysis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
-public class VraAnalysisApplication {
+public class VraAnalysisApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VraAnalysisApplication.class, args);
-	}
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(VraAnalysisApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            String dbName = mongoTemplate.getDb().getName();
+            System.out.println("----------------------------------------------");
+            System.out.println("CONNECTION SUCCESS: " + dbName);
+            System.out.println("----------------------------------------------");
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e.getMessage());
+        }
+    }
 }
