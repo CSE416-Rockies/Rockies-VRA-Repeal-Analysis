c64722f4ec3154bc7f6f63d39be9fdf9e25329cc
lee-H1208 <leeh120824@gmail.com>
2026-03-31 13:47:48 -0400

    Fixed legend and heatmap for Arkansas

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -170,10 +170,8 @@ export default function MapView(){
             let start = i === 0 ? min : bins[i - 1];
             let end = bin;
 
-            if(store.selectedState == 'Georgia'){
-                start = start*100;
-                end = end*100;
-            }
+            start = start*100;
+            end = end*100;
 
             return {
                 label: `${start.toFixed(1)}% - ${end.toFixed(1)}%`,
