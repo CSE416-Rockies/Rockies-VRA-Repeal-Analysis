120a51247331efe5bf97a41346a16e85021ecdc6
ces08 <christinesong08@gmail.com>
2026-05-04 10:12:33 -0400

    Handled misc state exception in URL

--- server/src/main/java/com/rockies/vra_analysis/GlobalExceptionHandler.java
+++ server/src/main/java/com/rockies/vra_analysis/GlobalExceptionHandler.java
@@ -4,15 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.rockies.vra_analysis.exceptions.StateDataNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(StateDataNotFoundException.class)
-
     ResponseEntity<Object> handleNotFound(StateDataNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    ResponseEntity<Object> handleTypeMismatch(MethodArgumentTypeMismatchException ex){
+        String msg = "Invalid state: '" + ex.getValue() + "'. Accepted values: AR, GA";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg); 
     }
     
 }
